fccf86d72c5184e848077581fdbd1b12bc73c9be
ppech <2861692+ppech@users.noreply.github.com>
2019-06-26 19:37:13 +0200

    added templating system

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,13 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
     debug = require('gulp-debug'),
-    cssmin = require('gulp-cssmin'),
-    ignore = require('gulp-ignore'),
+    //cssmin = require('gulp-cssmin'),
+    //ignore = require('gulp-ignore'),
     jsmin = require('gulp-jsmin'),
     plumber = require('gulp-plumber'),
     rename = require('gulp-rename'),
-    sourcemaps = require('gulp-sourcemaps');
+    nunjucksRender = require('gulp-nunjucks-render')
+sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('compile-sass', function (done) {
 
@@ -20,8 +21,44 @@ gulp.task('compile-sass', function (done) {
     done();
 });
 
+gulp.task('compile-js', function (done) {
+
+    gulp.src(['./scripts/*.js'])
+        .pipe(plumber())
+        .pipe(jsmin())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest("./docs/js/"));
+
+    done();
+});
+
+gulp.task('compile-html', function (done) {
+
+    gulp.src(['./pages/**/*.njk'])
+        .pipe(plumber())
+        .pipe(nunjucksRender({
+            path: './templates/' // String or Array
+        }))
+        .pipe(gulp.dest("./docs/"));
+
+    done();
+});
+
+
 gulp.task('watch-sass', function (done) {
     gulp.watch(['./styles/*.scss'], gulp.series('compile-sass'));
+
+    done();
+});
+
+gulp.task('watch-js', function (done) {
+    gulp.watch(['./scripts/*.js'], gulp.series('compile-js'));
+
+    done();
+});
+
+gulp.task('watch-html', function (done) {
+    gulp.watch(['./pages/**/*.njk'], gulp.series('compile-html'));
 
     done();
 });
